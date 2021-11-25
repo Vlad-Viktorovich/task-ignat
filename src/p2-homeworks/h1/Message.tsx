a083d8fd1d1ac3b47e70987764c5155b15b7ba4a
VladViltorovich <vizachem@gmail.com>
2021-11-25 14:34:32 +0300

    add h1 css/type

--- src/p2-homeworks/h1/Message.tsx
+++ src/p2-homeworks/h1/Message.tsx
@@ -1,9 +1,23 @@
 import React from 'react'
+import s from './Message.module.css'
 
-function Message() {
+type messagePropsType = {
+    avatar: string
+    name: string
+    message: string
+    time: string
+}
+
+function Message(props: messagePropsType) {
     return (
-        <div>
+        <div className={s.conteiner}>
+            <img src={props.avatar} alt="avatar" className={s.avatar}/>
 
+            <div className={s.text}>
+                <div className={s.name}>{props.name}</div>
+                <div className={s.message}>{props.message}</div>
+                <div className={s.time}>{props.time}</div>
+            </div>
         </div>
     )
 }
