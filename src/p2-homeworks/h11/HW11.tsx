05d5efa70ef03f8745953487d227799bcb153b05
VladViktorovich <vizachem@gmail.com>
2022-02-03 15:13:17 +0300

    add h11 Range

--- src/p2-homeworks/h11/HW11.tsx
+++ src/p2-homeworks/h11/HW11.tsx
@@ -3,37 +3,65 @@ import SuperRange from './common/c7-SuperRange/SuperRange'
 import SuperDoubleRange from './common/c8-SuperDoubleRange/SuperDoubleRange'
 
 function HW11() {
-    const [value1, setValue1] = useState(0)
-    const [value2, setValue2] = useState(100)
+  const [value1, setValue1] = useState(0)
+  const [value2, setValue2] = useState(100)
 
-    return (
-        <div>
-            <hr/>
-            homeworks 11
+  const colorStyle = () => {
+    if (value1 === 0) {
+      return {
+        color: 'navy'
+      }
+    } else if (value1 === 100) {
+      return {
+        color: 'red'
+      }
+    }
+  }
 
-            {/*should work (должно работать)*/}
-            <div>
-                <span>{value1}</span>
-                <SuperRange
-                    // сделать так чтоб value1 изменялось
-                />
-            </div>
+  const onChangeRangerSingle = (value: number) => {
+    if (value < value2 - 5) {
+      setValue1(value)
+    } else {
+      setValue1(value)
+      value <= 95 ? setValue2(value + 5) : setValue2(100)
+    }
+  }
 
-            <div>
-                <span>{value1}</span>
-                <SuperDoubleRange
-                    // сделать так чтоб value1 и value2 изменялось
-                />
-                <span>{value2}</span>
-            </div>
+  const onChangeDoubleRanger = (value: [number, number]) => {
+    setValue1(value[0])
+    setValue2(value[1])
+  }
 
-            <hr/>
-            {/*для личного творчества, могу проверить*/}
-            {/*<AlternativeSuperRange/>*/}
-            {/*<AlternativeSuperDoubleRange/>*/}
-            <hr/>
-        </div>
-    )
+  return (
+    <div>
+      <hr/>
+      <h2>homeworks 11</h2>
+
+      {/*should work (должно работать)*/}
+      <div>
+        <span style={colorStyle()}>{value1}</span>
+        <SuperRange
+          value={value1}
+          onChangeRange={onChangeRangerSingle}
+        />
+      </div>
+
+      <div style={{margin: '30px 0', display: 'flex'}}>
+        <span style={{color: value1 === 0 ? 'navy': ''}}>{value1}</span>
+        <SuperDoubleRange
+          value={[value1, value2]}
+          onChangeRange={onChangeDoubleRanger}
+        />
+        <span style={{color: value2 === 100 ? 'red': ''}}>{value2}</span>
+      </div>
+
+      <hr/>
+      {/*для личного творчества, могу проверить*/}
+      {/*<AlternativeSuperRange/>*/}
+      {/*<AlternativeSuperDoubleRange/>*/}
+      <hr/>
+    </div>
+  )
 }
 
 export default HW11
